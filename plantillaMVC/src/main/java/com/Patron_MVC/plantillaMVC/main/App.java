cd4f445be198be646fd0e946f57758c4b0be7268
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-10 22:49:25 +0200

    First Commit

---
+++ plantillaMVC/src/main/java/com/Patron_MVC/plantillaMVC/main/App.java
@@ -0,0 +1,13 @@
+package com.Patron_MVC.plantillaMVC.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
