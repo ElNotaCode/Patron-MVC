b859679df68fd2d08a685d255e43ef3bae7e8927
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-11 00:13:18 +0200

    Service creado

--- plantillaMVC/src/main/java/modelo/services/ClienteService.java
+++ plantillaMVC/src/main/java/modelo/services/ClienteService.java
@@ -1,8 +1,27 @@
 package modelo.services;
 
+import modelo.dao.ClienteDao;
+import modelo.dto.ClienteDto;
+
 public class ClienteService {
 
 	//Aquí va la comprobación.
 	//El service es el intermediario entre base de datos, controlador y el modelo.
+	
+	//Metodo que valida los datos del Create antes de pasarselos al DAO, pasamos
+	//por parametro el DTO.
+	public void validarCreate(ClienteDto clienteDto) {
+		
+		//Instanciamos el DAO.
+		ClienteDao clienteDao;
+		
+		//Aquí podríamos poner una condición.
+		
+		//? ClienteDao clienteDao = new ClienteDao();
+		clienteDao = new ClienteDao();
+		//Creamos al cliente.
+		clienteDao.createCliente(clienteDto);
+		
+	}
 	
 }
